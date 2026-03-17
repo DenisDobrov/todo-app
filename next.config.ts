588a7d2ad04e6c8ab9b644be70f73cb420865c35
Denis Dobrov <denis.dobrokhotov@gmail.com>
2026-03-17 08:13:17 -0300

    Small improvements and fixes

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   // Это увеличит лимит для всех Server Actions сразу
-  serverActions: {
-    bodySizeLimit: '10mb',
-  },
+
 }
 
 export default nextConfig;
